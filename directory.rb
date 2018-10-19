2d03e7d0a96ab017d241e02648eb08f8f74cfe44
Stuart Adair <stu@silverka.co.uk>
2018-10-19 14:37:31 +0100

    Refactored code to use CSV library

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 @students = []
 
 @menu_options =
@@ -110,24 +112,17 @@ def try_load_students(filename)
 end
 
 def load_students(filename)
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    add_student(name, cohort.to_sym)
+  CSV.foreach(filename) do |row|
+    add_student(row[0], row[1].to_sym)
   end
-  file.close
 end
 
 def save_students(filename)
-  # open the file for writing
-  file = File.open(filename, "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  CSV.open(filename, "w") do |csv_file|
+    @students.each do |student|
+      csv_file << [student[:name], student[:cohort]]
+    end
   end
-  file.close
 end
 
 def load_from_menu
